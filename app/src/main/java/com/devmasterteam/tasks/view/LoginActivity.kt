d266820767343ce7a57926e9492841d511f9700d
lucasgomes14 <lucazmatehus14@gmail.com>
2024-10-25 22:42:29 -0300

    Checking logged in user

--- app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
@@ -29,6 +29,9 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonLogin.setOnClickListener(this)
         binding.textRegister.setOnClickListener(this)
 
+        // Verificar se está logado
+        viewModel.verifyLoggedUser()
+
         // Observadores
         observe()
     }
@@ -46,6 +49,13 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 finish()
             } else {
                 Toast.makeText(applicationContext, it.message(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.loggedUser.observe(this) {
+            if (it) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
             }
         }
     }
