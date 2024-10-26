0c6a20f486efbd9c8b6e0dbac4b73fbefdf851b1
lucasgomes14 <lucazmatehus14@gmail.com>
2024-10-25 22:07:42 -0300

    Adding Authentication Data Header

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -10,6 +10,7 @@ import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
 import com.devmasterteam.tasks.service.repository.SecurityPreferences
+import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -28,6 +29,8 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
                 securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, result.token)
                 securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, result.personKey)
                 securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, result.name)
+
+                RetrofitClient.addHeaders(result.token, result.personKey)
 
                 _login.value = ValidationModel()
             }
